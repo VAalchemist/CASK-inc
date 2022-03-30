e32abb5adb6421f38d7034752bdc8da65d3d7b8d
EddieLo <eddarlostudy@yahoo.com>
2022-03-30 02:24:54 -0700

    app.get setup to connect/render to login and register page

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express()
 
+//used to connect to ejs dependencies
 app.set ('view-engine', 'ejs')
 
+//used to create intro homepage for login
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
+// used to connectç to login.ejs
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+// used to connect to register.ejs
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
 app.listen(3000)
