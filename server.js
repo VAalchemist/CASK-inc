e111b0480406caeb87801a6d8a0431128c1413e5
EddieLo <eddarlostudy@yahoo.com>
2022-03-30 03:36:22 -0700

    server js updated, bcrypt dependency

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express()
+// bcrypt used to hash private information to make sure app information is secure
+const bcrypt = require('bcrypt')
+
+// used to store user information in a local variable
+// temporalily used unless connected to database
+const users =[]
 
 //used to connect to ejs dependencies
 app.set ('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 
 //used to create intro homepage for login
 app.get('/', (req, res) => {
@@ -14,6 +21,11 @@ app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
 
+// used to connect to action of post in login.ejs
+app.post('/login', (req, res) => {
+
+})
+ 
 // used to connect to register.ejs
 app.get('/register', (req, res) => {
     res.render('register.ejs')
@@ -21,7 +33,8 @@ app.get('/register', (req, res) => {
 
 // used to connect to action of post in register.ejs
 app.post('/register', (req, res) => {
-
+    // using req.body connects to name='' fields under register.ejs
+    req.body.email
 })
 
 app.listen(3000)
